64d6a9e1340207a6098b21d38ce9056d85cbcb62
PSKOURO <102422130+PSKOURO@users.noreply.github.com>
2022-03-26 16:36:57 +0200

    Αρχικά αρχεία για έργο MakeCode

---
+++ main.ts
@@ -0,0 +1,9 @@
+basic.forever(function () {
+    basic.showString("Hello!")
+    basic.pause(5000)
+    basic.showString("GIOTA")
+    basic.pause(5000)
+    basic.showIcon(IconNames.LeftTriangle)
+    basic.clearScreen()
+    basic.pause(500)
+})
